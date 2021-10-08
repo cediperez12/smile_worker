f28f349dd3f6b90a6962da5f9722c233a66bfcd5
cediperez12 <34096750+cediperez12@users.noreply.github.com>
2021-10-08 20:04:00 +0800

    Report fragment created

--- app/src/main/java/com/smile/worker/Fragments/Navigation/fragment_nav_reports.java
+++ app/src/main/java/com/smile/worker/Fragments/Navigation/fragment_nav_reports.java
@@ -7,60 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputLayout;
 import com.smile.worker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link fragment_nav_reports#newInstance} factory method to
- * create an instance of this fragment.
- */
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class fragment_nav_reports extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @BindView(R.id.fr_nav_report_txtv_income)
+    TextView txtvIncome;
+    @BindView(R.id.fr_nav_report_txtv_jobs)
+    TextView txtvJobs;
+    @BindView(R.id.fr_nav_report_til_month)
+    TextInputLayout tilMonth;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private final String[] MONTHS = {"January","February","March","April",
+            "May","June","July","August","September","October","November","December"};
+
 
     public fragment_nav_reports() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment fragment_nav_reports.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static fragment_nav_reports newInstance(String param1, String param2) {
-        fragment_nav_reports fragment = new fragment_nav_reports();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_reports, container, false);
+        View v = inflater.inflate(R.layout.fragment_nav_reports, container, false);
+        ButterKnife.bind(this,v);
+
+        //To be continued with back-end. Meanwhile, search for a list.
+
+        init();
+
+        return v;
+    }
+
+    private void init() {
+        //Setup Month list
+        ArrayAdapter<String> monthlyListAdapter = new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_dropdown_item_1line,MONTHS);
+        AutoCompleteTextView monthlySpinner = (AutoCompleteTextView) tilMonth.getEditText();
+        monthlySpinner.setAdapter(monthlyListAdapter);
     }
 }
